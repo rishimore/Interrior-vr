070ea5dcf35a0eb25e0abccad40917c5011287f5
Rishi More <rishi.more9@gmail.com>
2019-10-13 22:11:15 +0530

    initial commit

---
+++ Interior VR/Assets/Scripts/EnableOrDisableVr.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class EnableOrDisableVr : MonoBehaviour
+{
+
+    public bool vrStatus;
+
+    void Awake(){
+
+        if(vrStatus == true){
+            UnityEngine.XR.XRSettings.enabled = true;
+        } else {
+            UnityEngine.XR.XRSettings.enabled = false;
+            Camera.main.GetComponent<Transform>().localRotation = UnityEngine.XR.InputTracking.GetLocalRotation(XRNode.CenterEye);
+        }
+
+        StartCoroutine(LoadDevice("Cardboard"));
+    }
+
+    IEnumerator LoadDevice(string deviceName)
+    {
+        UnityEngine.XR.XRSettings.LoadDeviceByName(deviceName);
+        yield return null;
+        if(vrStatus == true){
+            UnityEngine.XR.XRSettings.enabled = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vrStatus == false){
+            Camera.main.GetComponent<Transform>().localRotation = UnityEngine.XR.InputTracking.GetLocalRotation(XRNode.CenterEye);
+        }
+    }
+}
---
+++ Interior VR/Assets/Scripts/VrMainScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VrMainScene : MonoBehaviour {
+
+    void Awake(){
+        UnityEngine.XR.XRSettings.enabled = false;
+    }
+
+    public void LoadMagixBox(){
+        SceneManager.LoadScene (1);
+    }
+
+    public void LoadVrCardBoard(){
+        SceneManager.LoadScene (2);
+    }
+}
